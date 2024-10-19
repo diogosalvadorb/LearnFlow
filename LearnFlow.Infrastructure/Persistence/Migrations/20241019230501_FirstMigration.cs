b371432d218675a2c38b4830ff0fd5647910ee5f
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-10-19 20:09:45 -0300

    first migration

---
+++ LearnFlow.Infrastructure/Persistence/Migrations/20241019230501_FirstMigration.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnFlow.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assinaturas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duracao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assinaturas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Documento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefoneCelular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cover = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AssinaturaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cursos_Assinaturas_AssinaturaId",
+                        column: x => x.AssinaturaId,
+                        principalTable: "Assinaturas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioAssinaturas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AssinaturaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PagamentoAssinaturaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StatusAssinatura = table.Column<int>(type: "int", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataExpiracao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioAssinaturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsuarioAssinaturas_Assinaturas_AssinaturaId",
+                        column: x => x.AssinaturaId,
+                        principalTable: "Assinaturas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsuarioAssinaturas_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modulos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CursoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modulos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modulos_Cursos_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Cursos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PagamentoAssinaturas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataProcessamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StatusPagamento = table.Column<int>(type: "int", nullable: false),
+                    Mensagem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    UsuarioAssinaturaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LinkPagamento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExternoPagamentoId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataVencimento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PagamentoAssinaturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PagamentoAssinaturas_UsuarioAssinaturas_UsuarioAssinaturaId",
+                        column: x => x.UsuarioAssinaturaId,
+                        principalTable: "UsuarioAssinaturas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aulas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinkVideo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duracao = table.Column<int>(type: "int", nullable: false),
+                    ModuloId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aulas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Aulas_Modulos_ModuloId",
+                        column: x => x.ModuloId,
+                        principalTable: "Modulos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarioAulaConcluidas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AulaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataConclusao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Nota = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarioAulaConcluidas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_usuarioAulaConcluidas_Aulas_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Aulas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_usuarioAulaConcluidas_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aulas_ModuloId",
+                table: "Aulas",
+                column: "ModuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cursos_AssinaturaId",
+                table: "Cursos",
+                column: "AssinaturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modulos_CursoId",
+                table: "Modulos",
+                column: "CursoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PagamentoAssinaturas_UsuarioAssinaturaId",
+                table: "PagamentoAssinaturas",
+                column: "UsuarioAssinaturaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioAssinaturas_AssinaturaId",
+                table: "UsuarioAssinaturas",
+                column: "AssinaturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioAssinaturas_UsuarioId",
+                table: "UsuarioAssinaturas",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuarioAulaConcluidas_UsuarioId",
+                table: "usuarioAulaConcluidas",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PagamentoAssinaturas");
+
+            migrationBuilder.DropTable(
+                name: "usuarioAulaConcluidas");
+
+            migrationBuilder.DropTable(
+                name: "UsuarioAssinaturas");
+
+            migrationBuilder.DropTable(
+                name: "Aulas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Modulos");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+
+            migrationBuilder.DropTable(
+                name: "Assinaturas");
+        }
+    }
+}
